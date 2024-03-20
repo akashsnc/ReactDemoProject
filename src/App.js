496905c12218f2d21f6df44738559ef73c1b1682
Akash Suman <asuman@cdac.in>
2024-03-20 18:01:07 +0530

    render practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import React from 'react';
+import UseState from './render-components/UseState';
 
 function App() {
   return (
     <div className="App">
-        
+        <UseState/>
     </div>
   );
 }
