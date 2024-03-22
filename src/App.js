8fa49ca1502225412a2bab56def221defbeeb24c
Akash Suman <asuman@cdac.in>
2024-03-22 14:58:07 +0530

    render components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import React from 'react';
-import UseState from './render-components/UseState';
+import "./App.css";
+import React from "react";
+import UseState from "./render-components/UseState/UseState";
+import UseReducer from "./render-components/UseReducer/UseReducer";
 
 function App() {
   return (
     <div className="App">
-        <UseState/>
+      {/* <UseState /> */}
+      <UseReducer />
     </div>
   );
 }
